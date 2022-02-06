2a7ad73854d86f8139d01bbdeeeacedf02207ec1
AniketDasSushil <85302839+AniketDasSushil@users.noreply.github.com>
2022-02-06 20:50:45 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import yfinance as yf
+st.set_page_config(page_title='Stock Dashboard',
+                   page_icon=':bar_chart:',
+                   layout='wide'
+                   )
+ticker=['^nsei','tcs.ns','infy.ns','adanient.ns','irctc.ns','itc.ns']
+df = yf.download(ticker,period='1y')['Adj Close']
+select = st.sidebar.multiselect('select the stock',
+                       options=df.columns.values,
+                       default=df.columns.values)
+cum_s = st.sidebar.radio('Cummulative returns',['Yes','No'])
+pct = df.pct_change()
+cum = (1+pct).cumprod()
+if cum_s == 'Yes':
+    fig = px.line(cum[select],template='presentation')
+else:
+    fig = px.line(df[select],template='presentation')
+
+st.plotly_chart(fig)
+
